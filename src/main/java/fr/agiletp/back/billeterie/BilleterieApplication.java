e860d91f68609b0bc66a77f2a07a6fc09b7d7474
Colin Beaumont <colin.beaumon.26@gmail.com>
2023-05-26 14:28:45 +0200

    init java spring back

---
+++ src/main/java/fr/agiletp/back/billeterie/BilleterieApplication.java
@@ -0,0 +1,13 @@
+package fr.agiletp.back.billeterie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleterieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilleterieApplication.class, args);
+	}
+
+}
